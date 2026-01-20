8a3383912724f7b3debcc5eaa79a08437f9a1bbf
Yuqin Shi <shiyuqin@dfmc.com.cn>
2026-01-20 21:56:18 +0800

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1] ?? 'handsome-ke'
+const base = process.env.DEPLOY_ENV === 'GH_PAGES' ? `/${repoName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [vue()],
 })
